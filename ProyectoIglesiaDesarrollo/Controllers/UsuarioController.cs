9af4a8a761d758ac695530427d045ecc892b961e
Jonathan Soriano <jonathan.soriano@ujcv.edu.hn>
2024-07-16 23:26:58 -0600

    Modified UsuarioController

--- ProyectoIglesiaDesarrollo/Controllers/UsuarioController.cs
+++ ProyectoIglesiaDesarrollo/Controllers/UsuarioController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using ProyectoIglesiaDesarrollo.Models.Domain;
+using ProyectoIglesiaDesarrollo.Models.Domain.Entidades;
+using System.Text;
 
 namespace ProyectoIglesiaDesarrollo.Controllers
 {
@@ -10,8 +13,65 @@ namespace ProyectoIglesiaDesarrollo.Controllers
         {
             _context = context;
         }
-        public IActionResult Index()
+        public IActionResult Index(string Codigo)
         {
+            if (!string.IsNullOrWhiteSpace(Codigo))
+            {
+                ViewBag.Error = "Su Sesion Expiro";
+                return View(new UsuarioVm());
+            }
+            HttpContext.Session.Clear();
+            return View(new UsuarioVm());
+        }
+        [HttpPost]
+        public IActionResult Index(UsuarioVm vm)
+        {
+            var user = _context.Usuario
+                               .Where(w => w.Email == vm.Email)
+                               .ProjectToType<UsuarioVm>()
+                               .FirstOrDefault();
+            if (user == null)
+            {
+                ViewBag.Error = "Usuario o la contraseña no existen";
+                ViewBag.ClaseMensaje = "alert alert-warning alert-dismissable";
+                return View(new UsuarioVm());
+            }
+            if (user.Password != GetMD5(vm.Password))
+            {
+                ViewBag.Error = "Usuario o la contraseña no existen";
+                ViewBag.ClaseMensaje = "alert alert-warning alert-dismissable";
+                return View(new UsuarioVm());
+            }
+
+            var modulosroles = _context.modulosRoles
+                                       .Where(w => w.Eliminado == false && w.RolId == user.Rol.Id)
+                                       .ProjectToType<ModulosRoles>()
+                                       .ToList();
+
+            var agrupadosid = modulosroles.Select(s => s.Modulo.AgrupadoModuloId).Distinct().ToList();
+            var agrupados = _context.AgrupadoModulos
+                                    .Where(w => agrupadosid.Contains(w.Id))
+                                    .ProjectToType<AgrupadoVm>()
+                                    .ToList();
+
+            foreach (var item in agrupados)
+            {
+                var modulosactuales = modulosroles
+                                      .Where(w => w.Modulo.AgrupadoModuloId == item.Id)
+                                      .Select(s => s.Modulo.Id)
+                                      .Distinct()
+                                      .ToList();
+                item.Modulos = item.Modulos.Where(w => modulosactuales.Contains(w.Id)).ToList();
+            }
+
+            user.Menu = agrupados;
+            user.Password = string.Empty;
+
+            var sesionjson = JsonConvert.SerializeObject(user);
+            var plainTextBytes = Encoding.UTF8.GetBytes(sesionjson);
+            var sesionbase64 = Convert.ToBase64String(plainTextBytes);
+            HttpContext.Session.SetString("UsuarioObjeto", sesionbase64);
+
             return View();
         }
     }
