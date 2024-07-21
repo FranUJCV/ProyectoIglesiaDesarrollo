d248be0402d0f46725abee32df59505a449a4eec
fran8 <francisco.paz1@ujcv.edu,hn>
2024-07-21 17:33:05 -0600

    updated Program.cs

--- ProyectoIglesiaDesarrollo/Program.cs
+++ ProyectoIglesiaDesarrollo/Program.cs
@@ -7,6 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<IglesiaDbContext>
     (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession(opciones =>
+{
+    opciones.IdleTimeout = TimeSpan.FromMinutes(30);
+    opciones.Cookie.HttpOnly = true;
+    opciones.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
